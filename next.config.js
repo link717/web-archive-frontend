30ccb2f8e1fe878dd2dfe9d79cbc4c826e2b1613
link717 <ksy95433@gmail.com>
2023-05-23 15:27:20 +0900

    fix: CORS 오류 관련 rewrites 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/cdx/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/cdx/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
