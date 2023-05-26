1e7baefebc08e5658c1f4b2364e113bafe0ef5e9
link717 <ksy95433@gmail.com>
2023-05-26 23:30:00 +0900

    refactor: SEO 관련 title, 기본 meta 태그 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,10 @@ import React from 'react';
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <title>Web Archive</title>
+        <meta name="description" content="Web Archive를 사용한 토이 프로젝트" />
+      </Head>
       <body>
         <Main />
         <NextScript />
