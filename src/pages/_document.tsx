22ab70d7f120defc6cf3721d80d07f6a78cdef36
link717 <ksy95433@gmail.com>
2023-05-28 17:10:42 +0900

    fix: SSR 렌더링시 스타일 깜빡임 현상 방지 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,45 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+/* eslint-disable react/no-danger */
+import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs';
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 
-export default function Document() {
-  return (
-    <Html lang="ko">
-      <Head>
-        <title>Web Archive</title>
-        <meta name="description" content="Web Archive를 사용한 토이 프로젝트" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const cache = createCache();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          (
+            <StyleProvider cache={cache}>
+              <App {...props} />
+            </StyleProvider>
+          ),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style dangerouslySetInnerHTML={{ __html: extractStyle(cache) }} />
+        </>
+      ),
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="ko">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
